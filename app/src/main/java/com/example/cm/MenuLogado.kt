aa7b22ab43cf9fdd045ac89f0cba1294ad1e425c
prafaell <61152881+prafaell@users.noreply.github.com>
2021-03-04 16:00:12 +0000

    Ecra inicia,Ecra de login,Ecra de Utilizador com login feito

---
+++ app/src/main/java/com/example/cm/MenuLogado.kt
@@ -0,0 +1,15 @@
+package com.example.cm
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuLogado : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_logado)
+    }
+
+}
---
+++ app/src/main/java/com/example/cm/MenuLogin.kt
@@ -0,0 +1,11 @@
+package com.example.cm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_login)
+    }
+}
