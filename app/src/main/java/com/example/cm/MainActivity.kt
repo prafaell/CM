32c6c0b53b0d62120ce2d88d33625aee995df91b
prafaell <61152881+prafaell@users.noreply.github.com>
2021-03-08 14:37:35 +0000

    Butoes a funcionar

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun FunLogin(view: View) {
+        val intent = Intent(this,MenuLogin::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/cm/MenuLogin.kt
+++ app/src/main/java/com/example/cm/MenuLogin.kt
@@ -1,11 +1,18 @@
 package com.example.cm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MenuLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_login)
+    }
+
+    fun logar(view: View) {
+        val intent = Intent(this,MenuLogado::class.java)
+        startActivity(intent)
     }
 }
